86b699f79c5b7d426fd5f23471571808d365a626
tqvux <vuquangtrung2604@gmail.com>
2024-10-28 22:17:31 +0700

    asmweb3

--- dist/cartStorage.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cart = void 0;
-exports.saveCart = saveCart;
-exports.cart = JSON.parse(localStorage.getItem('cart') || '[]');
-function saveCart() {
-    localStorage.setItem('cart', JSON.stringify(exports.cart));
-}
---
+++ script.ts
@@ -0,0 +1,79 @@
+interface Product {
+    id: number;
+    name: string;
+    price: number;
+    image: string;
+    description: string;
+}
+
+let cart: Product[] = JSON.parse(localStorage.getItem('cart') || '[]');
+
+
+fetch('products.json')
+    .then(response => response.json())
+    .then((data: Product[]) => displayProducts(data));
+
+
+function displayProducts(products: Product[]) {
+    const productList = document.getElementById('product-list');
+    if (productList) {
+        productList.innerHTML = '';
+        products.forEach(product => {
+            const productDiv = document.createElement('div');
+            productDiv.className = 'product';
+            productDiv.innerHTML = `
+                <img src="${product.image}" alt="${product.name}">
+                <h2>${product.name}</h2>
+                <p>${product.description}</p>
+                <p>$${product.price}</p>
+                <button onclick="addToCart(${product.id})">Add to Cart</button>
+            `;
+            productList.appendChild(productDiv);
+        });
+    }
+}
+
+
+function addToCart(id: number) {
+    fetch('products.json')
+        .then(response => response.json())
+        .then((products: Product[]) => {
+            const product = products.find(p => p.id === id);
+            if (product) {
+                cart.push(product);
+                localStorage.setItem('cart', JSON.stringify(cart));
+                alert(`${product.name} has been added to the cart.`);
+            }
+        });
+}
+
+
+function displayCart() {
+    const cartList = document.getElementById('cart-list');
+    if (cartList) {
+        cartList.innerHTML = '';
+        cart.forEach((item, index) => {
+            const cartItemDiv = document.createElement('div');
+            cartItemDiv.className = 'cart-item';
+            cartItemDiv.innerHTML = `
+                <img src="${item.image}" alt="${item.name}">
+                <h2>${item.name}</h2>
+                <p>$${item.price}</p>
+                <button onclick="removeFromCart(${index})">Remove from Cart</button>
+            `;
+            cartList.appendChild(cartItemDiv);
+        });
+    }
+}
+
+
+function removeFromCart(index: number) {
+    cart.splice(index, 1);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    displayCart();
+}
+
+
+if (document.getElementById('cart-list')) {
+    displayCart();
+}
